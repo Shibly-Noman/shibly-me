00e54ee766cb5d498fc47a81d3be4479b616c418
Shibly Noman <shiblynoman20@gmail.com>
2020-10-14 10:55:37 +0600

    initial setup

---
+++ src/views/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-main>
+      <h1>This is good</h1>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
